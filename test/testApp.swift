cd168b25fee18257ec3f92e1a087e35fcbffc965
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-06-25 12:13:01 -0300

    :package: Simple Game with SwiftUI
    Following the tutorial:
    https://youtube.com/playlist?list=PLMRqhzcHGw1YqPh-ggQHJPAUxdHov_uNJ

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Luiz Carlos da Silva Araujo on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
