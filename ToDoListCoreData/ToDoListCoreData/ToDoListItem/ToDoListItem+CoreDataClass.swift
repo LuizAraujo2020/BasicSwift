66f17fda7d8423e133fc324fa7de5c137c446ee7
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-06-27 18:22:14 -0300

    :gift: Few updates

---
+++ ToDoListCoreData/ToDoListCoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListCoreData
+//
+//  Created by Luiz Carlos da Silva Araujo on 25/06/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
